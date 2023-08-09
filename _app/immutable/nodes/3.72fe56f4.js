8ac7dc3731ae3ce4aeea997ab3aab5e7d203c7c5
FlatErikk <FlatErikk@users.noreply.github.com>
2023-08-09 13:58:52 +0000

    deploy: 106f6047a8c162ec23f9fc84162d5ff580486634

---
+++ _app/immutable/nodes/3.72fe56f4.js
@@ -0,0 +1,61 @@
+import{S as K,i as Q,s as U,k as n,q as c,a as F,l as o,m as f,r as h,h as u,c as O,n as m,a1 as j,b as z,D as e,W as P}from"../chunks/index.58f29621.js";function J(G){let i,r,s,d,w,y,l,V,N,t,B,q,_,k,D,R,x,W,E,T,S,A,I,H,L,M;return{c(){i=n("div"),r=n("div"),s=n("div"),d=n("h1"),w=c("Mission statement"),y=F(),l=n("img"),N=F(),t=n("blockquote"),B=c(`At Nameleon, our mission is to protect and preserve the unique and fragile ecosystem of the
+        Namib Desert in Namibia, while promoting environmental stewardship and fostering a
+        sustainable future.
+        `),q=n("br"),_=n("br"),k=c(`
+        We are dedicated to the conservation of the Namaqua chameleons, a vital species within the
+        desert biome, and strive to raise awareness among road users about their importance and
+        vulnerability. By advocating for responsible and vigilant behavior on the roads to Sesriem out
+        of Walvis Bay and on the Boshua Pass exit out of Swakop towards Langer Heinrich mine, we
+        aim to mitigate the threats to these chameleons and safeguard their critical role in maintaining
+        the delicate balance of the desert ecosystem.
+        `),D=n("br"),R=n("br"),x=c(`
+        Furthermore, we are committed to monitoring the movement and well-being of key fauna and
+        flora species in the Namib Desert. Through our research and observations, we seek to better
+        understand the impacts of climate change and other environmental factors on this unique
+        ecosystem, enabling us to develop effective strategies for its preservation.
+        `),W=n("br"),E=n("br"),T=c(`
+        Education and awareness are central to our mission. We believe in empowering the youth
+        through educative tourism and outreach programs, equipping them with the knowledge and
+        tools necessary to become future environmental leaders. By instilling a sense of responsibility
+        and promoting sustainable practices, we aim to ensure the long-term protection of the Namib
+        Desert and its diverse flora and fauna.
+        `),S=n("br"),A=n("br"),I=c(`
+        Through collaboration with local communities, government agencies, and other stakeholders,
+        we strive to build partnerships that drive positive change. By fostering dialogue and engagingin advocacy efforts, we aim to influence policies and practices that promote the sustainable
+        management of natural resources and the mitigation of climate change impacts in Namibia.
+        `),H=n("br"),L=n("br"),M=c(`
+        At Nameleon, we are driven by our passion for the Namib Desert and its delicate ecology.
+        We envision a future where this unique ecosystem thrives, where the Namaqua chameleons
+        are safeguarded, and where the youth are empowered to become champions of environmental
+        conservation. Together, we can preserve the Namib Desert for future generations and inspire
+        a global commitment to protect and cherish our natural world.`),this.h()},l(g){i=o(g,"DIV",{});var b=f(i);r=o(b,"DIV",{class:!0});var v=f(r);s=o(v,"DIV",{class:!0});var p=f(s);d=o(p,"H1",{class:!0});var C=f(d);w=h(C,"Mission statement"),C.forEach(u),y=O(p),l=o(p,"IMG",{src:!0,alt:!0,width:!0}),p.forEach(u),N=O(v),t=o(v,"BLOCKQUOTE",{class:!0});var a=f(t);B=h(a,`At Nameleon, our mission is to protect and preserve the unique and fragile ecosystem of the
+        Namib Desert in Namibia, while promoting environmental stewardship and fostering a
+        sustainable future.
+        `),q=o(a,"BR",{}),_=o(a,"BR",{}),k=h(a,`
+        We are dedicated to the conservation of the Namaqua chameleons, a vital species within the
+        desert biome, and strive to raise awareness among road users about their importance and
+        vulnerability. By advocating for responsible and vigilant behavior on the roads to Sesriem out
+        of Walvis Bay and on the Boshua Pass exit out of Swakop towards Langer Heinrich mine, we
+        aim to mitigate the threats to these chameleons and safeguard their critical role in maintaining
+        the delicate balance of the desert ecosystem.
+        `),D=o(a,"BR",{}),R=o(a,"BR",{}),x=h(a,`
+        Furthermore, we are committed to monitoring the movement and well-being of key fauna and
+        flora species in the Namib Desert. Through our research and observations, we seek to better
+        understand the impacts of climate change and other environmental factors on this unique
+        ecosystem, enabling us to develop effective strategies for its preservation.
+        `),W=o(a,"BR",{}),E=o(a,"BR",{}),T=h(a,`
+        Education and awareness are central to our mission. We believe in empowering the youth
+        through educative tourism and outreach programs, equipping them with the knowledge and
+        tools necessary to become future environmental leaders. By instilling a sense of responsibility
+        and promoting sustainable practices, we aim to ensure the long-term protection of the Namib
+        Desert and its diverse flora and fauna.
+        `),S=o(a,"BR",{}),A=o(a,"BR",{}),I=h(a,`
+        Through collaboration with local communities, government agencies, and other stakeholders,
+        we strive to build partnerships that drive positive change. By fostering dialogue and engagingin advocacy efforts, we aim to influence policies and practices that promote the sustainable
+        management of natural resources and the mitigation of climate change impacts in Namibia.
+        `),H=o(a,"BR",{}),L=o(a,"BR",{}),M=h(a,`
+        At Nameleon, we are driven by our passion for the Namib Desert and its delicate ecology.
+        We envision a future where this unique ecosystem thrives, where the Namaqua chameleons
+        are safeguarded, and where the youth are empowered to become champions of environmental
+        conservation. Together, we can preserve the Namib Desert for future generations and inspire
+        a global commitment to protect and cherish our natural world.`),a.forEach(u),v.forEach(u),b.forEach(u),this.h()},h(){m(d,"class","content-heading"),j(l.src,V="static/images/space-shuttle-svgrepo-com.svg")||m(l,"src",V),m(l,"alt","..."),m(l,"width","40px"),m(s,"class","flex"),m(t,"class","content-blockquote"),m(r,"class","content-div")},m(g,b){z(g,i,b),e(i,r),e(r,s),e(s,d),e(d,w),e(s,y),e(s,l),e(r,N),e(r,t),e(t,B),e(t,q),e(t,_),e(t,k),e(t,D),e(t,R),e(t,x),e(t,W),e(t,E),e(t,T),e(t,S),e(t,A),e(t,I),e(t,H),e(t,L),e(t,M)},p:P,i:P,o:P,d(g){g&&u(i)}}}class Y extends K{constructor(i){super(),Q(this,i,null,J,U,{})}}export{Y as component};
